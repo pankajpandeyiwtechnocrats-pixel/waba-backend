dd06333559fe9580b7cc3ef83c12e665f8f876ed
pankajpandeyiwtechnocrats-pixel <pankajpandeyiwtechnocrats@gmail.com>
2026-02-06 17:31:00 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ const server = http.createServer(app);
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "https://waba-frontend-w8hu.vercel.app",
+    "https://waba-frontend-4.onrender.com/",
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
